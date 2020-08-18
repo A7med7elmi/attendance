7093c4489f3a939d6c0c12d06c415535e8663af8
Ahmed Helmy <ahmed.helmy@robustastudio.com>
2020-08-18 19:58:06 +0200

    change database server

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost:33076', 'root', 'secret', 'apsystem');
+	$conn = new mysqli('206.189.196.39:33076', 'root', 'secret', 'homestead3');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
