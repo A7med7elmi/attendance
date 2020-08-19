c23307784edc3bfbadddfb75e572ed68ad08b1f8
Ahmed Helmy <ahmed.helmy@robustastudio.com>
2020-08-19 19:23:48 +0200

    change header photo

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -7,7 +7,7 @@
       <span class="logo-lg"><b>Attendance </b>System</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar navbar-static-top" style="background-image: url('../images/banner.jpg');">
+    <nav class="navbar navbar-static-top">
       <!-- Sidebar toggle button-->
       <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
         <span class="sr-only">Toggle navigation</span>
